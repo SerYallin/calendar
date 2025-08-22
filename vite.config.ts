1f4ceeb592ac64f7ecd655552f10d6f346e4b8f4
Serhii Yallin <SerYallin@yandex.kz>
2025-08-22 16:21:35 +0300

    Update base config on vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/calendar/',
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src'),
